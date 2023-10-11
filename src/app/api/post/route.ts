52efc12ce3c008b71387a50a30b85c72c6b678ea
laludonisetiawan <laludonisetiawan@gmail.com>
2023-10-11 19:35:30 +0800

    membuat API POST di dalam direktori app/post/route.ts

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -4,5 +4,19 @@ import { NextRequest, NextResponse } from 'next/server';
 const prisma = new PrismaClient();
 export const GET = async (req: NextRequest) => {
   const posts = await prisma.post.findMany({});
-  return NextResponse.json({posts});
+  return NextResponse.json({ posts });
+};
+
+// menambahkan logic create data
+export const POST = async (req: NextRequest) => {
+  const { title, content } = await req.json();
+
+  const post = await prisma.post.create({
+    data: {
+      title,
+      content,
+    },
+  });
+
+  return NextResponse.json({post});
 };
